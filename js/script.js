e219dddc036af1403468d1e46e4d056a2e2e6910
Eclismic <milcokats@gmail.com>
2020-08-06 11:49:57 +0200

    website afgerond

--- js/script.js
+++ js/script.js
@@ -1,9 +1,59 @@
 $(document).ready(function() {
+    
     var getProductHeight = $('.project.active').height();
 
-    console.log(getProductHeight);
+
   
     $('.portfolio').css({
       height: getProductHeight
     });
+
+    function calcProductHeight() {
+      getProductHeight = $('.project.active').height();
+  
+      $('.portfolio').css({
+        height: getProductHeight
+      });
+    }
+
+    var projectItem = $('.project'),
+    projectCurrentItem = projectItem.filter('.active');
+
+
+  $('#next').on('click', function(e) {
+    e.preventDefault();
+
+    var nextItem = projectCurrentItem.next();
+
+    console.log(nextItem);
+
+    projectCurrentItem.removeClass('active');
+
+    console.log(projectCurrentItem);
+
+    if (nextItem.length) {
+      console.log(nextItem.length);
+      projectCurrentItem = nextItem.addClass('active');
+    } else {
+      projectCurrentItem = projectItem.first().addClass('active');
+    }
+      calcProductHeight();
+  });
+
+  $('#prev').on('click', function(e) {
+    e.preventDefault();
+
+    var prevItem = projectCurrentItem.prev();
+
+    projectCurrentItem.removeClass('active');
+
+    if (prevItem.length) {
+      projectCurrentItem = prevItem.addClass('active');
+    } else {
+      projectCurrentItem = projectItem.last().addClass('active');
+    }
+
+    calcProductHeight();
+  });
 });
+
